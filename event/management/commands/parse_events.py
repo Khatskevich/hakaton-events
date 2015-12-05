1f91847b49712f4742277292d6852537ffd50f33
AleksK <avkhatskevich@gmail.com>
2015-12-05 20:03:55 +0300

    example fixed

--- event/management/commands/parse_events.py
+++ event/management/commands/parse_events.py
@@ -4,17 +4,16 @@ from event.models import Event
 
 
 class Command(BaseCommand):
-    events = Event.objects.all() # all events
-    event = Event()#to get new instance
-    event.lat = 5
-    event.lng = 6
-    event.time = datetime.datetime.now()
-    #event.save() to save new ivent
 
     ### to add extra arguments
     #def add_arguments(self, parser):
     #    parser.add_argument('count', type=int)
 
     def handle(self, *args, **options):
-        pass
         #count = options['count']
+        events = Event.objects.all() # all events
+        event = Event()#to get new instance
+        event.lat = 5
+        event.lng = 6
+        event.time = datetime.datetime.now()
+        #event.save() to save new ivent
