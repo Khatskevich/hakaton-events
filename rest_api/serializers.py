208347a638e0a731a3b8af0c5fd6f9e7d59d49b6
AleksK <avkhatskevich@gmail.com>
2015-12-06 16:41:23 +0300

    new search queris added. Bug fixed

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -5,10 +5,10 @@ from event.models import Event
 
 class GetEventsSerializer(serializers.Serializer):
     sort_by = serializers.CharField(max_length=1, help_text='t=time, m=members', required=False, default='t')
-    lat_ne = serializers.FloatField(help_text="Latitude of the north east")
-    lng_ne = serializers.FloatField(help_text="Longitude of the north east")
-    lat_sw = serializers.FloatField(help_text="Latitude of the south west")
-    lng_sw = serializers.FloatField(help_text="Longitude of the south west")
+    lat_ne = serializers.FloatField(help_text="Latitude of the north east", default=60)
+    lng_ne = serializers.FloatField(help_text="Longitude of the north east", default=40)
+    lat_sw = serializers.FloatField(help_text="Latitude of the south west", default=50)
+    lng_sw = serializers.FloatField(help_text="Longitude of the south west", default=30)
 
 
 
